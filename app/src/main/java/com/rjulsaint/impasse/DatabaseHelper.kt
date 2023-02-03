af62772c807f6cf7852e79e8d7543239033a7f97
Ralph Julsaint <rjpro81@gmail.com>
2023-02-03 03:55:45 -0500

    Add initial master password and app bar view

--- app/src/main/java/com/rjulsaint/impasse/DatabaseHelper.kt
+++ app/src/main/java/com/rjulsaint/impasse/DatabaseHelper.kt
@@ -5,14 +5,16 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context : Context) : SQLiteOpenHelper(context, "ImpasseDatabase", null, 1) {
+    val table : String = "Password"
    override fun onCreate(db : SQLiteDatabase?){
-       val createPasswordTable = ("CREATE TABLE Password (id,webaddress,description,login,password,masterpassword)")
+       val createPasswordTable = ("CREATE TABLE Password (id INTEGER PRIMARY KEY AUTOINCREMENT,webaddress TEXT,description TEXT,login TEXT,password TEXT NOT NULL,masterpassword TEXT)")
        db?.execSQL(createPasswordTable)
-       val createMasterPasswordTable = ("CREATE TABLE MasterPassword (id,masterpassword)")
+       val createMasterPasswordTable = ("CREATE TABLE MasterPassword (id INTEGER PRIMARY KEY AUTOINCREMENT,masterpassword TEXT REFERENCES Password)")
        db?.execSQL(createMasterPasswordTable)
    }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        //"Not yet implemented"
+        db?.execSQL("DROP TABLE IF EXISTS $table")
+        onCreate(db)
     }
 }*/
--- app/src/main/java/com/rjulsaint/impasse/MainActivity.kt
+++ app/src/main/java/com/rjulsaint/impasse/MainActivity.kt
@@ -3,13 +3,20 @@ package com.rjulsaint.impasse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rjulsaint.impasse.ui.theme.ImPasseTheme
 //import android.database.*
 
@@ -20,25 +27,71 @@ class MainActivity : ComponentActivity() {
             ImPasseTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ImpassePreview()
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DisplayMasterPassword() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Master Password:", color = Color.Blue, style = TextStyle(color = Color.Blue))
+            var text = "Enter here"
+            TextField(
+                value = text,
+                textStyle = TextStyle(color = Color.White),
+                onValueChange = { text = it },
+                enabled = true,
+                shape = AbsoluteRoundedCornerShape(corner = CornerSize(15.dp)),
+                colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Blue)
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(top = 4.dp, bottom = 4.dp)
+            )
+            Text(text = "Skip for now", color = Color.Cyan, style = TextStyle(color = Color.Cyan, textDecoration = TextDecoration.Underline))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ImpassePreview() {
     ImPasseTheme {
-        Greeting("Android")
+        Scaffold(
+            topBar = {
+                TopAppBar (
+                      backgroundColor = Color.Blue
+                        ){
+                    Text(
+                        "Impasse",
+                        modifier = Modifier.padding(start = 15.dp),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+            }
+        ) { contentPadding ->
+            Box(modifier = Modifier.padding(contentPadding))
+            DisplayMasterPassword()
+        }
     }
 }
